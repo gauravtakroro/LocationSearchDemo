b11608839d358d7c9c2e522b39508214939d2a68
Gaurav Tak <gaurav.tak@roro.io>
2023-08-06 15:57:17 +0530

    Initial Commit

---
+++ LocationSearchDemo/LocationSearchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchDemoApp.swift
+//  LocationSearchDemo
+//
+//  Created by Roro Solutions LLP on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
